d7b1b8d328b76598a6368592152c5f4970ac7212
Miles <sino_miles@foxmail.com>
2018-01-03 02:03:36 +0800

    JDBC Demo

---
+++ src/com/miles/Test.java
@@ -0,0 +1,57 @@
+package com.miles;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//数据库连接
+		Connection conn=null;
+		try {
+			//加载MYSQL驱动
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			//拿到一个数据库连接
+			conn =DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "root");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		String sql="select * from student";
+		try {
+			Statement statement = conn.createStatement();
+			//结果
+			ResultSet  resultSet=  statement.executeQuery(sql);
+			while(resultSet.next())
+			{
+				int id = resultSet.getInt(1);
+				String name = resultSet.getString(2);
+				int age = resultSet.getInt(3);
+				System.out.println("id:"+id+" 姓名:"+name+" 年龄:"+age);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			if (conn!=null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+		}
+		
+	}
+
+}
